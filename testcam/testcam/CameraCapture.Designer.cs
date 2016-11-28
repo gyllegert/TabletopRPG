30d8a425e8f17cf5ef82408ee6cfb46c00a0901a
dalnoeh <kbejer15@student.aau.dk>
2016-11-28 13:21:23 +0100

    Basics
    What was done before git

---
+++ testcam/testcam/CameraCapture.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace testcam
+{
+    partial class CameraCapture
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.captureBoxOrg = new Emgu.CV.UI.ImageBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.captureBoxBlue = new Emgu.CV.UI.ImageBox();
+            this.captureBoxMagenta = new Emgu.CV.UI.ImageBox();
+            this.captureBoxGreen = new Emgu.CV.UI.ImageBox();
+            this.captureBoxTeal = new Emgu.CV.UI.ImageBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.captureBoxOrg)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.captureBoxBlue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.captureBoxMagenta)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.captureBoxGreen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.captureBoxTeal)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // captureBoxOrg
+            // 
+            this.captureBoxOrg.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.captureBoxOrg.Location = new System.Drawing.Point(12, 12);
+            this.captureBoxOrg.Name = "captureBoxOrg";
+            this.captureBoxOrg.Size = new System.Drawing.Size(512, 384);
+            this.captureBoxOrg.TabIndex = 2;
+            this.captureBoxOrg.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(1145, 28);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // captureBoxBlue
+            // 
+            this.captureBoxBlue.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.captureBoxBlue.Location = new System.Drawing.Point(542, 12);
+            this.captureBoxBlue.Name = "captureBoxBlue";
+            this.captureBoxBlue.Size = new System.Drawing.Size(512, 384);
+            this.captureBoxBlue.TabIndex = 4;
+            this.captureBoxBlue.TabStop = false;
+            // 
+            // captureBoxMagenta
+            // 
+            this.captureBoxMagenta.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.captureBoxMagenta.Location = new System.Drawing.Point(542, 443);
+            this.captureBoxMagenta.Name = "captureBoxMagenta";
+            this.captureBoxMagenta.Size = new System.Drawing.Size(512, 384);
+            this.captureBoxMagenta.TabIndex = 5;
+            this.captureBoxMagenta.TabStop = false;
+            // 
+            // captureBoxGreen
+            // 
+            this.captureBoxGreen.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.captureBoxGreen.Location = new System.Drawing.Point(12, 443);
+            this.captureBoxGreen.Name = "captureBoxGreen";
+            this.captureBoxGreen.Size = new System.Drawing.Size(512, 384);
+            this.captureBoxGreen.TabIndex = 6;
+            this.captureBoxGreen.TabStop = false;
+            // 
+            // captureBoxTeal
+            // 
+            this.captureBoxTeal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.captureBoxTeal.Location = new System.Drawing.Point(1072, 443);
+            this.captureBoxTeal.Name = "captureBoxTeal";
+            this.captureBoxTeal.Size = new System.Drawing.Size(512, 384);
+            this.captureBoxTeal.TabIndex = 7;
+            this.captureBoxTeal.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(1132, 84);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox1.TabIndex = 8;
+            this.richTextBox1.Text = "";
+            // 
+            // CameraCapture
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1584, 985);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.captureBoxTeal);
+            this.Controls.Add(this.captureBoxGreen);
+            this.Controls.Add(this.captureBoxMagenta);
+            this.Controls.Add(this.captureBoxBlue);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.captureBoxOrg);
+            this.Name = "CameraCapture";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.CameraCapture_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.captureBoxOrg)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.captureBoxBlue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.captureBoxMagenta)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.captureBoxGreen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.captureBoxTeal)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox captureBoxOrg;
+        private System.Windows.Forms.Button btnStart;
+        private Emgu.CV.UI.ImageBox captureBoxBlue;
+        private Emgu.CV.UI.ImageBox captureBoxMagenta;
+        private Emgu.CV.UI.ImageBox captureBoxGreen;
+        private Emgu.CV.UI.ImageBox captureBoxTeal;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ testcam/testcam/CameraCapture.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using Emgu;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.Util;
+using Emgu.CV.Cvb;
+
+namespace testcam
+{
+    public partial class CameraCapture : Form
+    {
+        Capture capture = null;
+        bool captureInProgress;
+
+        Image<Hsv, Byte> ImgHsv;
+        Image<Gray, Byte> ImgBlue;
+        Image<Gray, Byte> ImgGreen;
+        Image<Gray, Byte> ImgTeal;
+        Image<Gray, Byte> ImgMagenta;
+
+        public CameraCapture()
+        {
+            InitializeComponent();
+        }
+
+        private void CameraCapture_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                capture = new Capture();
+                capture.SetCaptureProperty(CapProp.FrameWidth, 640);
+                capture.SetCaptureProperty(CapProp.FrameHeight, 512);
+                capture.SetCaptureProperty(CapProp.AutoExposure, 0);
+                //capture.SetCaptureProperty(CapProp.Exposure, 0.1);
+            }
+            catch (NullReferenceException excpt)
+            {
+
+                MessageBox.Show(excpt.Message);
+            }
+
+            if (capture != null)
+            {
+                if (captureInProgress)
+                {  //if camera is getting frames then stop the capture and set button Text
+                    // "Start" for resuming capture
+                    btnStart.Text = "Start";
+                    Application.Idle -= ProcessFrame;
+                }
+                else
+                {
+                    //if camera is NOT getting frames then start the capture and set button
+                    // Text to "Stop" for pausing capture
+                    btnStart.Text = "Stop";
+                    Application.Idle += ProcessFrame;
+                }
+
+                captureInProgress = !captureInProgress;
+            }
+            richTextBox1.AppendText("Width is:" + capture.GetCaptureProperty(CapProp.FrameWidth) + "Height is:" + capture.GetCaptureProperty(CapProp.FrameHeight));
+        }
+
+        private void ProcessFrame(object sender, EventArgs arg)
+        {
+            Mat temp = capture.QueryFrame();
+            Image<Bgr, Byte> ImageFrame = temp.ToImage<Bgr, byte>();
+
+            ImgHsv = ImageFrame.Convert<Hsv, Byte>();
+
+            CvInvoke.MedianBlur(ImgHsv, ImgHsv, 9);
+
+            ImgBlue = FindRange(ImgHsv, 95, 105, 100, 255, 80, 255);
+            ImgGreen = FindRange(ImgHsv, 60, 80, 0, 255, 150, 255);
+            ImgTeal = FindRange(ImgHsv, 80, 90, 0, 255, 150, 255);
+            ImgMagenta = FindRange(ImgHsv, 110, 130, 0, 255, 150, 255);
+
+            int blobAreaMinSize = 500;
+
+            captureBoxOrg.Image = ImageFrame;
+            captureBoxBlue.Image = FindBlobs(ImgBlue, 0, 0, 255, blobAreaMinSize);
+            captureBoxGreen.Image = FindBlobs(ImgGreen, 0, 255, 0, blobAreaMinSize);
+            captureBoxTeal.Image = FindBlobs(ImgTeal, 0, 255, 255, blobAreaMinSize);
+            captureBoxMagenta.Image = FindBlobs(ImgMagenta, 255, 0, 255, blobAreaMinSize);
+        }
+
+        public Image<Gray, byte> FindRange(Image<Hsv, byte> ImgHsv, int HMin, int HMax, int VMin, int VMax, int SMin, int SMax)
+        {
+            //Split input HSV img into separate channels
+            Image<Gray, Byte>[] channels = ImgHsv.Split();  
+            Image<Gray, Byte> imgHue = channels[0];            
+            Image<Gray, Byte> imgSat = channels[1];
+            Image<Gray, Byte> imgVal = channels[2];
+
+            //Filter out colors that aren't inside the given range
+            Image<Gray, byte> huefilter = imgHue.InRange(new Gray(HMin), new Gray(HMax));
+
+            //Filter out low saturation colors
+            Image<Gray, byte> satfilter = imgSat.InRange(new Gray(SMin), new Gray(SMax));
+
+            //Filter out colors that aren't bright
+            Image<Gray, byte> valfilter = imgVal.InRange(new Gray(VMin), new Gray(VMax));
+
+            //Put the three channels back together
+            Image<Gray, byte> FImg = huefilter.And(valfilter).And(satfilter);
+
+            return FImg;
+        }
+
+        public Image<Rgb, byte> FindBlobs(Image<Gray, byte> imgGray, int r, int g, int b, int minBlobArea)
+        {
+
+            CvBlobDetector detector = new CvBlobDetector();
+            CvBlobs resultingBlobs = new CvBlobs();
+
+            uint numBlobsFound = detector.Detect(imgGray, resultingBlobs);
+
+            Image<Rgb, byte> blobImg = imgGray.Convert<Rgb, byte>();
+            Rgb val = new Rgb(r, g, b);
+
+            foreach (CvBlob targetBlob in resultingBlobs.Values)
+            {
+                if (targetBlob.Area > minBlobArea)
+                {
+                    blobImg.Draw(targetBlob.BoundingBox, val, 1);
+                }
+            }
+
+            return blobImg;
+        }
+
+        public Image<Hsv, byte> FindColor(Image<Hsv, byte> ImgHsv, int HMin, int HMax, int saturation, int value)
+        {
+            for (int i = 0; i < ImgHsv.Cols; i++)
+            {
+                for (int j = 0; j < ImgHsv.Rows; j++)
+                {
+                    if (ImgHsv.Data[j, i, 0] > HMin && ImgHsv.Data[j, i, 0] < HMax && ImgHsv.Data[j, i, 1] > saturation && ImgHsv.Data[j, i, 2] > value)
+                    {
+                        ImgHsv.Data[j, i, 0] = 0;
+                        ImgHsv.Data[j, i, 1] = 0;
+                        ImgHsv.Data[j, i, 2] = 255;
+                    }
+                    else
+                    {
+                        ImgHsv.Data[j, i, 0] = 0;
+                        ImgHsv.Data[j, i, 1] = 0;
+                        ImgHsv.Data[j, i, 2] = 0;
+                    }
+                }
+            }
+            return ImgHsv;
+        }
+
+        public Image<Hsv, byte> FindRed(Image<Hsv, byte> ImgHsv, int HMin, int HMax, int HMin2, int HMax2, int saturation, int value)
+        {
+            for (int i = 0; i < ImgHsv.Cols; i++)
+            {
+                for (int j = 0; j < ImgHsv.Rows; j++)
+                {
+                    if (((ImgHsv.Data[j, i, 0] > HMin && ImgHsv.Data[j, i, 0] < HMax) ||
+                        (ImgHsv.Data[j, i, 0] > HMin2 && ImgHsv.Data[j, i, 0] < HMax2)) && ImgHsv.Data[j, i, 1] > saturation && ImgHsv.Data[j, i, 2] > value)
+                    {
+                        ImgHsv.Data[j, i, 0] = 0;
+                        ImgHsv.Data[j, i, 1] = 0;
+                        ImgHsv.Data[j, i, 2] = 255;
+                    }
+                    else
+                    {
+                        ImgHsv.Data[j, i, 0] = 0;
+                        ImgHsv.Data[j, i, 1] = 0;
+                        ImgHsv.Data[j, i, 2] = 0;
+                    }
+                }
+            }
+            return ImgHsv;
+        }
+
+        private void ReleaseData()
+        {
+            if (capture != null)
+                capture.Dispose();
+        }
+
+
+        
+    }
+}
